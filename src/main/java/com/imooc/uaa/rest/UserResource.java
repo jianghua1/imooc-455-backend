e159ee809b91cfa94474db1bb5092700a6a827e1
jianghua1 <719584530@qq.com>
2024-11-10 21:28:49 +0800

    2-1 认证和授权的概念 第一部分

---
+++ src/main/java/com/imooc/uaa/rest/UserResource.java
@@ -0,0 +1,17 @@
+package com.imooc.uaa.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserResource {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello World";
+    }
+
+}
